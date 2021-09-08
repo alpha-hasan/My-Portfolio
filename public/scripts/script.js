a1b3baa1203a7dc950d9dba3c43ec46aca23038b
Alpha <hasanrizwan6@gmail.com>
2021-09-08 22:46:46 +0500

    Favicon Added

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -14,22 +14,34 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show");
         }
     });
-    // slide-up script
+
+
+    // // slide-up script
+
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
     $('.navbar .menu li a').click(function(){
+
+
         // applying again smooth scroll on menu items click
+
         $('html').css("scrollBehavior", "smooth");
     });
+
+
     // toggle menu/navbar script
+    
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
+    
     // typing text animation script
+
     var typed = new Typed(".typing-1", {
         strings: ["السلام عليكم", "Hello", "你好", "नमस्ते"],
         typeSpeed: 100,
@@ -48,7 +60,10 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true
     });
+
+
     // owl carousel script
+
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
