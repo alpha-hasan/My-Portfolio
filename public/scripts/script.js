b44e28921cc2ae293be9fb67525d7395d216f2c0
hasan <hasanrizwan6@gmail.com>
2023-02-11 05:40:10 +0500

    probably last refacto

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
@@ -18,12 +18,12 @@ $(document).ready(function(){
 
     // // slide-up script
 
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
 
 
         // applying again smooth scroll on menu items click
@@ -33,13 +33,13 @@ $(document).ready(function(){
 
 
     // toggle menu/navbar script
-    
-    $('.menu-btn').click(function(){
+
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    
+
     // typing text animation script
 
     var typed = new Typed(".typing-1", {
@@ -49,13 +49,13 @@ $(document).ready(function(){
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["React Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-3", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["React Developer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,15 +71,15 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
